0bf01545550ea89f3f70505f9087fcc4cca722da
mflowers1395 <60019181+mflowers1395@users.noreply.github.com>
2020-04-20 16:33:55 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 function init(){
 
-var input = document.getElementById('entryinput');
+var message = document.getElementById('entryinput');
+
+var name = document.getElementsByClassName('center') [1];
 
 var button = document.getElementById('entrybutton');
 
 var output = document.getElementById('textoutput');
 
 
-button.addEventListener('click', function(){
-  output.innerHTML = input.value;
-  alert('Marilyn Elaine Flowers: ' + input.value)
-});
+function alertsFunction(){
+  alert(name.textContent + ': ' + message.value);
+}
 
+function displayFunction(){
+  display.innerHTML = message.value;
+}
+
+button.addEventListener('click', displayFunction);
+button.addEventListener('click'), alertsFunction);
 }
 
 
